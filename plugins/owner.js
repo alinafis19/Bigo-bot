ecfcd7686599ca2fa1b2f230e78f5331ea07080f
alinafis19 <alinafis123a@gmail.com>
2025-01-21 23:33:06 +0100

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,17 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
-  let caption = `
-*「 معلومات عن صاحب البوت 」*\n\n
-*Whatsapp channel:*\n https://whatsapp.com/channel/0029VaX4b6J7DAWqt3Hhu01A\n
-*instagram:*\ninstagram.com/noureddine_ouafy
-
-*youtube:*\nyoutube.com/@noureddineouafy2
-
-*facebook page:*\nwww.facebook.com/profile.php?id=100063533185520
-
-*script bot :* github.com/noureddineouafy
-
-`.trim()
+  let caption = `+212719558797`.trim()
   m.reply(caption)
 }
 handler.help = ['owner']
